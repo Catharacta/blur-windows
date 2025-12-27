c416b94e8ab71a2f6e43c0479d84636ae9ffc76a
ym <utmiya@gmail.com>
2025-12-27 13:47:50 +0900

    Update .gitignore to exclude build artifacts and local handovers, and remove tracked target directories

--- blur-windows-rs/target/debug/examples/basic-08c468c3559075ad.d
+++
@@ -1,5 +0,0 @@
-C:\Users\atuya\Documents\develop\blur-windows\blur-windows-rs\target\debug\examples\basic-08c468c3559075ad.d: examples\basic.rs
-
-C:\Users\atuya\Documents\develop\blur-windows\blur-windows-rs\target\debug\examples\libbasic-08c468c3559075ad.rmeta: examples\basic.rs
-
-examples\basic.rs:
--- blur-windows-rs/target/debug/examples/basic.d
+++
@@ -1,5 +0,0 @@
-C:\Users\atuya\Documents\develop\blur-windows\blur-windows-rs\target\debug\examples\basic.d: examples\basic.rs
-
-C:\Users\atuya\Documents\develop\blur-windows\blur-windows-rs\target\debug\examples\basic.exe: examples\basic.rs
-
-examples\basic.rs:
